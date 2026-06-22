d03a9d538e122677e6359b5928bd406b57ffd181
michaelochieng0 <mclay@kabarak.ac.ke>
2026-06-22 09:55:31 +0300

    made sure the imports for dotenv and loading it comes first in the server.js and also in the aiservice made sure that the api key lading/initialization is inside the async function so it is loaded second

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
+import dotenv from "dotenv"
+dotenv.config();
 import express from "express"
 import cors from "cors"
-import dotenv from "dotenv"
 import aiRoutes from "./routes/ai.routes.js"
 
 dotenv.config();
--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -1,11 +1,11 @@
 import { GoogleGenAI } from "@google/genai";
 
-const ai = new GoogleGenAI({
-    apiKey: process.env.GEMINI_API_KEY,
-});
-
 export const generateAIResponse = async(prompt)=>{
     try{
+        const ai = new GoogleGenAI({
+        apiKey: process.env.GEMINI_API_KEY,
+    });
+
         const response=await
         client.chat.completions.create({
         model: "gemini-2.5-flash",
