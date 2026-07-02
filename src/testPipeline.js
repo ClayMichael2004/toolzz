449a527385860eeffc83c088ea1da48f02dffc75
michaelochieng0 <mclay@kabarak.ac.ke>
2026-07-02 10:56:17 +0300

    refactor: changing the file we are looking at because of the change in local pcs and work pc not having postman and the zip i was using

--- src/testPipeline.js
+++ src/testPipeline.js
@@ -1,7 +1,7 @@
 import { analyzeProject } from "./services/projectAnalysis.service.js";
 
 const result = await analyzeProject(
-    "src/temp/c615d656-45f4-4f1f-a9fd-cb6b5ed3b261"
+    "src/temp/frontend"
 );
 
 console.dir(result, { depth: null });
